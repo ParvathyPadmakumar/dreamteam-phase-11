4134f89ffb8539cc375aaabac8b3bf3edf85a3fc
ParvathyPadmakumar <parvathypadmakumar03@gmail.com>
2026-03-12 23:45:54 +0530

    fix: Resolved link format issues in searchgithub.py

--- task#2/Chapter13/searchgithub.py
+++ task#2/Chapter13/searchgithub.py
@@ -1,15 +1,22 @@
-import requests,sys,bs4,webbrowser
+headers={"User-Agent":"Mozilla/5.0"}
+import requests,sys,bs4,webbrowser,os
 if len(sys.argv)<2:
-    print('Usage: searchgithub.py [search keyword]')
+    print('Usage:searchgithub.py keyword')
     sys.exit()
-search=''.join(sys.argv[1:])
-response=requests.get("https://github.com/search?q="+search)
+repos=[]
+search='+'.join(sys.argv[1:])
+response=requests.get("https://github.com/search?q="+search,headers=headers)
 response.raise_for_status()
 soup=bs4.BeautifulSoup(response.text,'html.parser')
-repos=soup.select('a.v-align-middle')
+links = soup.select('a[href^="/"]')
+for l in links:
+    href=l.get('href')
+    if href.count("/") == 2:
+        repos.append(href)
 if len(repos)==0:
     print('No results found')
 else:
     results=min(5,len(repos))
     for i in range(results):
-        webbrowser.open('https://github.com'+repos[i].get('href'))
+        webbrowser.open('https://github.com'+repos[i])
+
